357c9ccfc524fc42da8e55ffbce213c3265e48b2
Elle Dashfield <eleanor.dashfield@gmail.com>
2020-02-29 09:16:25 +1300

    Intial setup of project

---
+++ readme.md
@@ -0,0 +1,20 @@
+## Automated tests for the Cimate Mind beta contributor page
+
+This is a demo Ruby project showing the use of RSpec (a Ruby test framework) and Capybara (a Selenium Webdriver wrapper) to test a submission form page at https://scientistsspeakup.org/climate-mind/
+
+You may find the tests run slowly due to the relatively slow speed of the site.
+
+I will use the FactoryBot gem to create factories and the Faker gem to generate random data.
+
+To run this project you will need
+* Ruby 2.1.2 or greater
+* bundler
+
+To install bundler:\
+```gem install bundler```
+
+To install the gems:\
+```bundle install```
+
+To run the tests:\
+```bundle exec rspec```
---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+require "bundler"
+require "capybara/dsl"
+require "capybara/rspec"
+
+Bundler.setup(:default)
+Bundler.require
+
+# Which default browser do you want Selenium WebDriver to use?
+# :selenium_chrome # Selenium driving Chrome
+# :selenium_chrome_headless # Selenium driving Chrome in a headless configuration
+# https://github.com/teamcapybara/capybara#selenium
+
+Capybara.default_driver = :selenium_chrome
+Capybara.app_host = "https://scientistsspeakup.org/climate-mind/"
+Capybara.default_max_wait_time = 5
+# Disable Capybara's rack server as we are testing an external site
+Capybara.run_server = false
+
+# Run using:
+# bundle exec rspec spec/features/login.rb --format documentation
+# ... Or...
+
+RSpec.configure do |config|
+  # use verbose test results output
+  config.formatter = :documentation
+end
